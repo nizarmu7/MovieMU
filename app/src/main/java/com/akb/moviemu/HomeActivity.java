cf693408c310704348e65a2c240bffaf8e569ac8
Your Name <nizarmu7@gmail.com>
2020-06-25 13:20:29 +0700

    Penambahan BottomNavigation

--- app/src/main/java/com/akb/moviemu/HomeActivity.java
+++ app/src/main/java/com/akb/moviemu/HomeActivity.java
@@ -1,17 +1,62 @@
 package com.akb.moviemu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+import com.akb.moviemu.Fragment.AboutFragment;
+import com.akb.moviemu.Fragment.HomeFragment;
+import com.akb.moviemu.Fragment.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // kita set default nya Home Fragment
+        loadFragment(new HomeFragment());
+        // inisialisasi BottomNavigaionView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    // method listener untuk logika pemilihan
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.search_menu:
+                fragment = new SearchFragment();
+                break;
+            case R.id.about_menu:
+                fragment = new AboutFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 
     @Override
@@ -43,5 +88,4 @@ public class HomeActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
-
 }
