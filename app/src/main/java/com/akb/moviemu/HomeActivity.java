d351b86963b06daeb4052ebdd49648547b049da5
Your Name <nizarmu7@gmail.com>
2020-06-25 13:45:37 +0700

    Perbaikan

--- app/src/main/java/com/akb/moviemu/HomeActivity.java
+++ app/src/main/java/com/akb/moviemu/HomeActivity.java
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 import com.akb.moviemu.Fragment.AboutFragment;
 import com.akb.moviemu.Fragment.HomeFragment;
